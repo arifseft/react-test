b35c612d9b9e7f49c17ac652b46cfb4c3ce17c4e
M. Arif Sefrianto <arifsefrianto@gmail.com>
2020-05-16 12:45:24 +0700

    using hooks

--- src/App.js
+++ src/App.js
@@ -1,22 +1,57 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import "./App.css";
 import WeatherCard from "./components/WeatherCard/component";
 
 function App() {
-  const data = async () => {
+  const [query, setQuery] = useState("Jakarta, ID");
+  const [weather, setWeather] = useState({
+    temp: null,
+    condition: null,
+    city: null,
+    country: null,
+  });
+
+  const data = async (q) => {
     const apiRes = await fetch(
-      "http://api.openweathermap.org/data/2.5/weather?q=London,uk&unit=metric&APPID=51971f23667365b046c16ad6b2d76dfa"
+      `http://api.openweathermap.org/data/2.5/weather?q=${q}&units=metric&APPID=51971f23667365b046c16ad6b2d76dfa`
     );
-	const resJSON = await apiRes.json();
-	return resJSON;
+    const resJSON = await apiRes.json();
+    return resJSON;
   };
-  console.log(data());
+  const handleSearch = (e) => {
+    e.preventDefault();
+    data(query).then((res) => {
+      setWeather({
+        temp: res.main.temp,
+        condition: res.weather[0].main,
+        country: res.sys.country,
+        city: res.name,
+      });
+    });
+  };
+  useEffect(() => {
+    data(query).then((res) => {
+      setWeather({
+        temp: res.main.temp,
+        condition: res.weather[0].main,
+        country: res.sys.country,
+        city: res.name,
+      });
+    });
+  }, [query]);
   return (
     <div className="App">
-      <WeatherCard temp={30} condition="Clear" city="Jakarta" country="ID" />
-      <WeatherCard temp={13} condition="Rain" city="Jakarta" country="ID" />
-      <WeatherCard temp={2} condition="Tornado" city="Jakarta" country="ID" />
+      <WeatherCard
+        temp={weather.temp}
+        condition={weather.condition}
+        city={weather.city}
+        country={weather.country}
+      />
+      <form action="">
+        <input value={query} onChange={(e) => setQuery(e.target.value)} />
+        <button onClick={(e) => handleSearch(e)}>Search</button>
+      </form>
     </div>
   );
 }
